e7df5ec9fcd727ae093b4a58b3c8d859c0463d6f
Janamejay Sharma <janamejay.mindstormer@gmail.com>
2023-03-25 20:26:53 -0600

    Changes to comments

--- image-classifier.py
+++ image-classifier.py
@@ -1,8 +1,9 @@
-from tensorflow.keras import datasets, layers, models
+from tensorflow import keras
+from keras import datasets, models, layers
 
 """
-    datasets.cifar10.load_data() returns (x_train, y_train), (x_test, y_test), where:
-    
+    The function call datasets.cifar10.load_data() returns:
+        (x_train, y_train), (x_test, y_test), where:
     x_train: uint8 NumPy array of grayscale image data with shapes (50000, 32, 32, 3), containing the training data. Pixel values range from 0 to 255.
     y_train: uint8 NumPy array of labels (integers in range 0-9) with shape (50000, 1) for the training data.
     x_test: uint8 NumPy array of grayscale image data with shapes (10000, 32, 32, 3), containing the test data. Pixel values range from 0 to 255.
@@ -30,7 +31,6 @@ model = models.Sequential()
 
 """
     Add a convolution layer with 32 neurons (filters, or feature maps)
-    Feature map size: 3x3
     Type: rectified linear unit (ReLU)
     Input: 32x32 pixel image with 3 color channels 
 """ 
@@ -59,22 +59,18 @@ model.add(layers.Conv2D(filters=64, kernel_size=(3,3), activation='relu'))
 # Flatten input to one dimension
 model.add(layers.Flatten())
 
-"""
-    Dense implements the operation: output = activation(dot(input, kernel) + bias),
-    where activation is the element-wise activation function passed as the activation argument
-"""
+# Performs the operation: output = activation(dot(input, kernel) + bias)
 model.add(layers.Dense(units=64, activation='relu'))
 
 """
-    Output layer, 10 units for each class of image
-    Performs the function: output = activation(dot(input, kernel) + bias)
+    Output layer, 10 units, 1 for each class of image
     'softmax' normalizes output so all values sum to 1
     This is useful since we want the probability of an image belonging to a class
 """
 model.add(layers.Dense(units=10, activation='softmax'))
 
 """
-    Loss function is used to measure error or deviation
+    Loss function measures error or deviation
     "sparse" allows only non-zero values to be added to the matrix of labels
 """
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
